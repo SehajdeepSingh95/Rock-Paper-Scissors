b806c453586c3a0385521309aa5929e776ba3152
Sehajdeep Singh <sehajdeepsdsn@gmail.com>
2025-07-21 10:14:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let your_score = 0;
+let computer_score = 0;
+let draw_score = 0;
+
+function computer_choice() {
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function makeMove(playerChoice) {
+  let computerChoice = computer_choice();
+
+  document.querySelector('.moves-chosen').innerHTML = `
+    You 
+    <img src="images/${playerChoice}-emoji.png" alt="Your Move">
+    <img src="images/${computerChoice}-emoji.png" alt="Computer Move">
+    Computer
+  `;
+
+  let result = "";
+
+  if (playerChoice === computerChoice) {
+    result = "It's a Draw!";
+    draw_score++;
+  } else if (
+    (playerChoice === 'rock' && computerChoice === 'scissors') ||
+    (playerChoice === 'paper' && computerChoice === 'rock') ||
+    (playerChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    result = "You Win!";
+    your_score++;
+  } else {
+    result = "Computer Wins!";
+    computer_score++;
+  }
+
+  document.querySelector('.overall-result').innerHTML = `
+    Wins: ${your_score}, 
+    Losses: ${computer_score}, 
+    Draws: ${draw_score}
+  `;
+}
+
+function resetScore() {
+  your_score = 0;
+  computer_score = 0;
+  draw_score = 0;
+  document.querySelector('.overall-result').innerHTML = `
+    Wins: 0, Losses: 0, Draws: 0
+  `;
+  document.querySelector('.moves-chosen').innerHTML = '';
+}
